a39ba10d9698c729dc0e0c1850a4d1495758b979
MASUMHD <mrmasum2k4@gmail.com>
2024-09-29 17:59:33 +0600

    update error page

--- src/Component/SharePage/Error.jsx
+++ src/Component/SharePage/Error.jsx
@@ -1,29 +1,18 @@
+import Lottie from "lottie-react";
+import { Link } from "react-router-dom";
+import ErrorAnimation from "../../assets/2404.json";
+
 const Error = () => {
   return (
     <div>
-      <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
-        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-          <div className="max-w-md text-center">
-            <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-              <span className="sr-only">Error</span>404
-            </h2>
-            <p className="text-2xl font-semibold md:text-3xl">
-              Sorry, we could not find this page.
-            </p>
-            <p className="mt-4 mb-8 dark:text-gray-600">
-              But dont worry, you can find plenty of other things on our
-              homepage.
-            </p>
-            <a
-              rel="noopener noreferrer"
-              href="/"
-              className="px-8 py-3 font-semibold rounded dark:bg-default-600 dark:text-gray-50 btn btn-info"
-            >
-              Back to homepage
-            </a>
-          </div>
+      <div className="w-full absolute  h-screen md:h-screen flex flex-col justify-center items-center space-y-5">
+        <Lottie animationData={ErrorAnimation} />
+        <div className="text-center absolute bottom-10">
+          <button className="primary-btn text-whit bg-[#F7A582] font-bold py-2 px-4 rounded hover:bg-[#f2936a] ">
+            <Link to="/">Back to Home</Link>
+          </button>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
